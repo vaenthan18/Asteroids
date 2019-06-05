084273bc0eba0d1eb77214dff100893536aac35a
Daniel Zhang <zhang.daniel.d@gmail.com>
2019-06-04 20:18:28 -0400

    Edited to create a class in gameboard to handle creation of the main menu rather than having a method within asteroids

--- src/asteroids/AsteroidsGUI.java
+++ src/asteroids/AsteroidsGUI.java
@@ -8,13 +8,13 @@ import asteroids.components.GameComponent;
 import asteroids.components.gameitems.*;
 import asteroids.listeners.MoveListener;
 import asteroids.listeners.MouseClickListener;
+import asteroids.components.gameboard.MainMenu;
 
 import java.util.Random;
 
 public final class AsteroidsGUI extends JPanel implements Runnable {
 
     private JFrame frame = new JFrame("Asteroids");
-    private JPanel menuPanel = new JPanel();
     private JPanel mainPanel = new JPanel(); //Layout
     private ArrayList<GameComponent> components = new ArrayList<>();
     private ArrayList<Bullet> bulletList = new ArrayList<>();
@@ -39,7 +39,7 @@ public final class AsteroidsGUI extends JPanel implements Runnable {
         //When the start button is clicked the menu is deleted and the game is added
         running = true;
         frame.getContentPane().removeAll();
-        frame.remove(menuPanel);
+        frame.remove(frame.getContentPane());
         frame.add(this);
         frame.setContentPane(this);
         frame.repaint();
@@ -88,48 +88,8 @@ public final class AsteroidsGUI extends JPanel implements Runnable {
         frame.setSize(frameLength, frameHeight);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Start with the menu
-        makeMenu(this);
+        frame.add(new MainMenu(frame, this));
         frame.setFocusable(true);
-    }
-
-    public void makeMenu(AsteroidsGUI gui) {
-        //Menu creation
-        menuPanel.setLayout(null);
-        menuPanel.setBackground(bgc);
-        frame.add(menuPanel);
-
-        JLabel title = new JLabel("ASTEROIDS", SwingConstants.CENTER);
-        title.setBounds(350, 100, 300, 100);
-        title.setForeground(Color.WHITE);
-        title.setFont(new Font("Courier", Font.PLAIN, 55));
-
-        MouseClickListener buttonPressed = new MouseClickListener(gui);
-
-        JButton startButton = new JButton("START");
-        startButton.setBounds(450, 350, 100, 50);
-        startButton.setBackground(Color.BLACK);
-        startButton.setForeground(Color.WHITE);
-        startButton.setOpaque(true);
-        startButton.setBorder(BorderFactory.createLineBorder(Color.WHITE, 1));
-        startButton.setFont(new Font("Courier", Font.PLAIN, 20));
-        startButton.addActionListener(buttonPressed);
-        startButton.setActionCommand("start");
-
-        JButton scoreButton = new JButton("SCORES");
-        scoreButton.setBounds(450, 425, 100, 50);
-        scoreButton.setBackground(Color.BLACK);
-        scoreButton.setForeground(Color.WHITE);
-        scoreButton.setOpaque(true);
-        scoreButton.setBorder(BorderFactory.createLineBorder(Color.WHITE, 1));
-        scoreButton.setFont(new Font("Courier", Font.PLAIN, 20));
-        scoreButton.addActionListener(buttonPressed);
-        scoreButton.setActionCommand("score");
-
-        JLabel shopButton = new JLabel("Shop");
-
-        menuPanel.add(startButton);
-        menuPanel.add(scoreButton);
-        menuPanel.add(title);
     }
 
     public void paintComponent(Graphics g) {
---
+++ src/asteroids/components/gameboard/MainMenu.java
@@ -0,0 +1,52 @@
+package asteroids.components.gameboard;
+
+
+import asteroids.AsteroidsGUI;
+import asteroids.listeners.MouseClickListener;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu extends JPanel {
+
+    public static Color bgc = Color.decode("#240046");
+
+    public MainMenu(JFrame frame, AsteroidsGUI gui) {
+        //Menu creation
+        this.setLayout(null);
+        this.setBackground(bgc);
+
+        JLabel title = new JLabel("ASTEROIDS", SwingConstants.CENTER);
+        title.setBounds(350, 100, 300, 100);
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Courier", Font.PLAIN, 55));
+
+        MouseClickListener buttonPressed = new MouseClickListener(gui);
+
+        JButton startButton = new JButton("START");
+        startButton.setBounds(450, 350, 100, 50);
+        startButton.setBackground(Color.BLACK);
+        startButton.setForeground(Color.WHITE);
+        startButton.setOpaque(true);
+        startButton.setBorder(BorderFactory.createLineBorder(Color.WHITE, 1));
+        startButton.setFont(new Font("Courier", Font.PLAIN, 20));
+        startButton.addActionListener(buttonPressed);
+        startButton.setActionCommand("start");
+
+        JButton scoreButton = new JButton("SCORES");
+        scoreButton.setBounds(450, 425, 100, 50);
+        scoreButton.setBackground(Color.BLACK);
+        scoreButton.setForeground(Color.WHITE);
+        scoreButton.setOpaque(true);
+        scoreButton.setBorder(BorderFactory.createLineBorder(Color.WHITE, 1));
+        scoreButton.setFont(new Font("Courier", Font.PLAIN, 20));
+        scoreButton.addActionListener(buttonPressed);
+        scoreButton.setActionCommand("score");
+
+        JLabel shopButton = new JLabel("Shop");
+
+        this.add(startButton);
+        this.add(scoreButton);
+        this.add(title);
+    }
+}
