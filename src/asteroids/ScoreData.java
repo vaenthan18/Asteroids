4f513a0f29fb3cd1f38efd03998019ff4f03b950
Daniel Zhang <zhang.daniel.d@gmail.com>
2019-06-05 00:56:08 -0400

    Scoreboard - incomplete, adding txt

---
+++ src/asteroids/ScoreData.java
@@ -0,0 +1,27 @@
+package asteroids;
+
+import java.util.Comparator;
+
+public class ScoreData implements Comparable<ScoreData>{
+
+    private int score;
+    private String name;
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ScoreData(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(ScoreData scores) {
+        return (scores.score - this.score);
+    }
+}
